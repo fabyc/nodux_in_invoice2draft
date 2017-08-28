8320471f38706cab61072ab4008ce54e93a0cdfc
Tatiana Quizhpe <tatianaq@nodux.ec>
2017-08-28 12:44:49 -0500

    Modify in invoice with withholding

--- invoice.py
+++ invoice.py
@@ -21,6 +21,11 @@ from trytond.wizard import Wizard, StateAction, StateView, StateTransition, \
 __all__ = ['InInvoicetoDraftStart', 'InInvoicetoDraft']
 __metaclass__ = PoolMeta
 
+class Invoice():
+    'Invoice'
+    __name__ = 'account.invoice'
+
+
 class InInvoicetoDraftStart(ModelView):
     'In Invoice to Draft Start'
     __name__ = 'nodux.account.in.invoice.to.draft.start'
@@ -42,6 +47,8 @@ class InInvoicetoDraft(Wizard):
         pool = Pool()
         Sale = pool.get('sale.sale')
         Invoice = pool.get('account.invoice')
+        Withholding = pool.get('account.withholding')
+        VoucherLine = pool.get('account.voucher.line')
         invoices = Invoice.browse(Transaction().context['active_ids'])
         ModelData = pool.get('ir.model.data')
         User = pool.get('res.user')
@@ -66,17 +73,32 @@ class InInvoicetoDraft(Wizard):
                 self.raise_user_error('No tiene permiso para modificar la factura %s', invoice.number)
             else:
                 if invoice.type == "in_invoice":
+
                     if invoice.ref_withholding != "":
-                        self.raise_user_error('No puede reversar una factura de compra que ya tiene Retencion autorizada por el SRI')
-                    else:
-                        if invoice.move:
-                            cursor.execute('DELETE FROM account_move_line WHERE move = %s' %invoice.move.id)
-                            cursor.execute('DELETE FROM account_move WHERE id = %s' %invoice.move.id)
-                            #for line in invoice.lines:
-                                #cursor.execute('DELETE FROM account_invoice_line WHERE id = %s' %line.id)
-                            #cursor.execute('DELETE FROM account_invoice WHERE id = %s' %invoice.id)
-                        invoice.state = "draft"
-                        invoice.move = None
-                        invoice.save()
+                        withholdings = Withholding.search([('number','=', invoice.ref_withholding)])
+                        if withholdings:
+                            for withholding in withholdings:
+                                # cursor.execute('DELETE FROM account_withholding_tax WHERE withholding = %s' %withholding.id)
+                                # cursor.execute('DELETE FROM account_withholding WHERE id =%s' %withholding.id)
+                                withholding.state = "annulled"
+                                withholding.save()
+
+                    if invoice.move:
+                        cursor.execute('DELETE FROM account_move_line WHERE move = %s' %invoice.move.id)
+                        cursor.execute('DELETE FROM account_move WHERE id = %s' %invoice.move.id)
+                    #for line in invoice.lines:
+                        #cursor.execute('DELETE FROM account_invoice_line WHERE id = %s' %line.id)
+                    #cursor.execute('DELETE FROM account_invoice WHERE id = %s' %invoice.id)
+                    if invoice.state == 'paid':
+                        voucher_line = VoucherLine.search([('name','=', invoice.number)])
+                        cursor.execute('DELETE FROM account_voucher_line WHERE name =%s' %invoice.number)
+                        cursor.execute('DELETE FROM account_voucher WHERE ud =%s' %voucher_line.voucher)
+                        cursor.execute('DELETE FROM account_move_line WHERE move = %s' %voucher_line.voucher.move.id)
+                        cursor.execute('DELETE FROM account_move WHERE id = %s' %voucher_line.voucher.move.id)
+                    invoice.number = None
+                    invoice.state = "draft"
+                    invoice.move = None
+                    invoice.ref_withholding = None
+                    invoice.save()
                 else:
                     self.raise_user_error('No puede modificar una factura de venta. Dirijase a Ventas->Ventas TPV')
